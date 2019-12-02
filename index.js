cc623419acb74a646689f0b19c2433504a254d27
JChartier001 <JChartier001@gmail.com>
2019-12-02 16:28:14 -0600

    CRUD operations working backend

--- index.js
+++ index.js
@@ -70,6 +70,30 @@ server.delete('/users/:id', (req, res) => {
     })
 })
 
+server.put('/users/:id', (req, res) => {    
+    const userData = req.body;
+    const id = req.params.id
+    const name = req.params.name
+    const bio =req.params.bio
+    const account = db.findById((userId) => userId.id === id);
+
+    db.update(id, userData)
+    .then(user => {
+        if(!account){
+            res.status(404).json({message: "The user with the specified ID does not exist."})
+        } else if(user.name || user.bio == "") {
+            res.status(400).json({message: "Please provide name and bio for the user."})
+        }else {
+            res.status(200).json(userData)
+        }
+    })
+    .catch(error => {
+        console.log("Error updating user", error);
+        res.status(500).json({message: "The user information could not be modified."})
+
+    })
+})
+
 
 const port = 4000;
 server.listen(port, () => {
