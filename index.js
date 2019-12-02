a7b54a95ad6437e6a06761637ee5d5cb3eef622f
JChartier001 <JChartier001@gmail.com>
2019-12-02 13:44:21 -0600

    server running.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send({api: "up and running"});
+});
+
+server,get('/api/users', (req, res) => {
+    db.find()
+    .then()
+    .catch(error => {
+        console.log("error on GET /hubs", error)
+        res.status(404).json({message: "error getting users from database"})
+    })
+})
+
+
+const port = 4000;
+server.listen(port, () => {
+    console.log(`\n ** API running on port ${port} **\n`)
+});
