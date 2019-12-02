d3dd2a11f2263683534322c9af2d181d73d79f32
JChartier001 <JChartier001@gmail.com>
2019-12-02 14:48:30 -0600

    post success and post error working

--- index.js
+++ index.js
@@ -4,19 +4,51 @@ const express = require('express');
 const server = express();
 
 server.use(express.json());
+const db = require('./data/db.js')
 
 server.get('/', (req, res) => {
     res.send({api: "up and running"});
 });
 
-server,get('/api/users', (req, res) => {
+server.post('/users', (req, res) => {
+const userData = req.body;
+
+    db.insert(userData)
+    .then(added => {
+        if(added.name || added.bio === null){
+            res.status(400).json({message: "Please provide name and bio for the user."});       
+        } else {
+            res.status(201).json(userData);            
+        }
+    })
+    .catch(error => {
+        console.log("error adding new user", error);
+        res.status(500).json({message: "There was an error while saving the user to the database"});
+    });
+});
+
+server.get('/users', (req, res) => {
     db.find()
-    .then()
+    .then(users => {
+        res.status(200).json(users);
+    })
     .catch(error => {
         console.log("error on GET /hubs", error)
-        res.status(404).json({message: "error getting users from database"})
+        res.status(500).json({message: "The users information could not be retrieved."})
     })
 })
+
+server.get('/users/:id', (req, res) => {
+    const id = req.params.id
+    db.findById(id)
+    .then(user => {
+        res.status(200).json(user);
+    })
+    .catch(error => {
+        console.log("error on GET /hubs", error)
+        res.status(404).json({message: "error getting users from database"});
+    });
+});
 
 
 const port = 4000;
